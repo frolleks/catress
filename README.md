4b761e82cfb15f60224d15b40c58031a8d085c84
frolleks <frolleks@hotmail.com>
2023-03-03 22:50:40 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# catress
+
+A simple command line tool to read files.
+
+## Usage
+
+To read a file simply run:
+
+```sh
+catress file.txt
+```
+
+And, to read multiple files simply run:
+
+```sh
+catress file1.txt file2.txt
+```
---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+use std::fs;
+
+// Define a custom error type
+#[derive(Debug)]
+struct CustomError {
+    message: String,
+}
+
+impl CustomError {
+    fn new(message: &str) -> CustomError {
+        CustomError {
+            message: message.to_string(),
+        }
+    }
+}
+
+impl Display for CustomError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for CustomError {}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() == 1 {
+        let file_path = &args[0];
+        let contents =
+            fs::read_to_string(&file_path).map_err(|err| CustomError::new(&err.to_string()))?;
+        println!("{}", contents);
+    } else {
+        for file_path in args {
+            let contents =
+                fs::read_to_string(&file_path).map_err(|err| CustomError::new(&err.to_string()))?;
+
+            println!("Contents of file '{}':\n{}", file_path, contents);
+        }
+    }
+
+    Ok(())
+}
